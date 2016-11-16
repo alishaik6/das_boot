0875ad480dd2997586e8ed0a8482fc63b3d041a3
mohammadali6 <ali546838@gmail.com>
2016-11-16 15:34:16 -0600

    added changes for both embedded tomcat and container

--- das-boot/src/main/java/com/boot/App.java
+++ das-boot/src/main/java/com/boot/App.java
@@ -2,19 +2,42 @@ package com.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 
 /**
  * Hello world!
  *
  */
-@SpringBootApplication
-//@EnableAutoConfiguration(exclude=DataSourceAutoConfiguration.class)
-public class App 
+// for embedded tomcat
+//@SpringBootApplication
+////@EnableAutoConfiguration(exclude=DataSourceAutoConfiguration.class)
+//public class App 
+//{
+//    public static void main( String[] args )
+//    {
+//        SpringApplication.run(App.class, args);
+//    }
+//}	
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+
+public class App  extends SpringBootServletInitializer
 {
     public static void main( String[] args )
     {
         SpringApplication.run(App.class, args);
     }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<App> applicationClass = App.class;
 }
